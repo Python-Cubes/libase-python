d7d5cd0a1a8e65c16e13ec6243a23ac6ecc9c432
Fasion Chan <fasionchan@gmail.com>
2018-05-07 11:01:43 +0800

    add trace and eval_now

---
+++ libase/decorator/helper.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- encoding=utf8 -*-
+
+'''
+FileName:   helper.py
+Author:     Fasion Chan
+@contact:   fasionchan@gmail.com
+@version:   $Id$
+
+Description:
+
+Changelog:
+
+'''
+
+
+def eval_now(func):
+    '''
+        Call function and pass back return value immediately.
+
+        Arguments:
+            func - Function to call
+    '''
+
+    return func()
